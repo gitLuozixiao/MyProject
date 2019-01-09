19b2b6bcbd1495bad911a2c99cdffd477d7bf637
luozixiao <g.luozixiao@gmail.com>
2019-01-09 17:10:59 +0800

    initial

---
+++ js/myJs.js
@@ -0,0 +1,12 @@
+/**
+ * Created by zixiao on 2019/1/9.
+ */
+$(document).ready(function(){
+    $("#to_top").click(function(){
+        //点击“回到顶部”，滚动到顶部，并带动画效果
+        $("html,body").animate({scrollTop:0},1000);
+    });
+    $('.cell img').zoomify();
+    $('.page img').zoomify();
+    $('.illustration img').zoomify();
+});
