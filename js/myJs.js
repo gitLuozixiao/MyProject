57cfb232ab71eafd6c34f74507c3f92747366153
luozixiao <g.luozixiao@gmail.com>
2019-01-30 13:34:44 +0800

    change title

--- js/myJs.js
+++ js/myJs.js
@@ -5,7 +5,7 @@
 $(document).ready(function(){
     $("#to_top").click(function(){
         //点击“回到顶部”，滚动到顶部，并带动画效果
-        $("html,body").animate({scrollTop:0},1000);
+        $("html,body").animate({scrollTop:0},500);
     });
     $('.cell img').zoomify();
     $('.page img').zoomify();
